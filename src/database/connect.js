10c4ebee1003f8c12ffd85d999207c1683a72c86
mateusamarall <mateus.amaral018@gmail.com>
2021-06-27 10:01:03 -0300

    removido arquivos desnecessários

--- src/database/connect.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var typeorm_1 = require("typeorm");
-/*
-verifica se existe o
-arquivo ormconfig e pega os dados
-de la para fazer a conexão */
-typeorm_1.createConnection().then(function () { return console.log('connected with db'); });
