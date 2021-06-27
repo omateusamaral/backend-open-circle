82d97d46029cfd0edefe0a5d905259d27c1ba5c0
mateusamarall <mateus.amaral018@gmail.com>
2021-06-27 10:29:49 -0300

    configuraçao de deploy da aplicaçao na heroku

--- dist/app.js
+++ dist/app.js
@@ -3,12 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
 require("reflect-metadata");
 var express_1 = __importDefault(require("express"));
 var helmet_1 = __importDefault(require("helmet"));
 var cors_1 = __importDefault(require("cors"));
 require("./database/connect");
 var routes_1 = __importDefault(require("./routes/usersRoutes/routes"));
+dotenv_1.default.config();
 var App = /** @class */ (function () {
     function App() {
         this.app = express_1.default();
